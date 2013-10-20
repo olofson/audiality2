d9050356408175b2adc76cc2934507bdb338813d
David Olofson <david@olofson.net>
2013-10-20 03:57:57 +0200

    Command line player a2play
    Added a rudimentary command line player. It will try to play "Song"
    by default, but the -p switch allows you to play any program in
    the loaded module, with arguments.
    
    There are also some basic object introspection capabilities; the -x
    and -xr switches recursively list all objects in the last loaded
    module or engine root respectively, printing out type, period, size
    etc of waves, and I/O port counts and control registers of units.
    
    Closes #20, though there is no file writing support yet.

---
+++ a2play/a2play.c
@@ -0,0 +1,400 @@
+/*
+ * a2play.c - Audiality 2 command line player
+ *
+ * Copyright 2013 David Olofson <david@olofson.net>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include "audiality2.h"
+#include "waves.h"
+#include "units.h"
+
+
+/* Configuration */
+static char *audiodriver = NULL;
+static int samplerate = 44100;
+static int channels = 2;
+static int audiobuf = 4096;
+static int a2flags = A2_REALTIME | A2_EXPORTALL | A2_RTERRORS | A2_TIMESTAMP;
+
+/* State and control */
+static A2_state *state = NULL;	/* Engine state*/
+static A2_handle module = -1;	/* Handle of last loaded module */
+static int do_exit = 0;
+
+
+static void print_info(int indent, const char *xname, A2_handle h)
+{
+	int i;
+	A2_handle x;
+	A2_otypes t = a2_TypeOf(state, h);
+	const char *name = a2_Name(state, h);
+	int has_exports = a2_GetExport(state, h, 0) >= 1;
+	if(has_exports)
+	{
+		for(i = 0; i < indent; ++i)
+			printf("    ");
+		printf("---------------------------------------------\n");
+	}
+	for(i = 0; i < indent; ++i)
+		printf("    ");
+	if(xname)
+		printf("%-16s", xname);
+	else if(name)
+		printf("%-16s", name);
+	else
+		printf("%-16d", h);
+	printf("%-12s", a2_TypeName(state, t));
+	switch(t)
+	{
+	  case A2_TBANK:
+		break;
+	  case A2_TWAVE:
+	  {
+		A2_wave *w = a2_GetWave(state, h);
+		switch(w->type)
+		{
+		  case A2_WOFF:
+			printf("OFF     ");
+			break;
+		  case A2_WNOISE:
+			printf("NOISE   ");
+			break;
+		  case A2_WWAVE:
+			printf("WAVE    ");
+			break;
+		  case A2_WMIPWAVE:
+			printf("MIPWAVE ");
+			break;
+		}
+		switch(w->type)
+		{
+		  case A2_WOFF:
+			break;
+		  case A2_WNOISE:
+			printf(" per: %-8d", w->period);
+			break;
+		  case A2_WWAVE:
+		  case A2_WMIPWAVE:
+			printf(" per: %-8d size: %-8d", w->period,
+					w->d.wave.size[0]);
+			if(w->flags & A2_LOOPED)
+				printf(" LOOPED");
+			break;
+		}
+		break;
+	  }
+	  case A2_TPROGRAM:
+		/* TODO: New properties for argument count, defaults etc! */
+		break;
+	  case A2_TUNIT:
+	  {
+		const A2_crdesc *crd;
+		const A2_unitdesc *ud = a2_GetUnitDescriptor(state, h);
+
+		/* Inputs and outputs */
+		if(ud->maxinputs)
+		{
+			if(ud->mininputs == ud->maxinputs)
+				printf("i: %d     ", ud->mininputs);
+			else
+				printf("i: %d..%d  ", ud->mininputs,
+						ud->maxinputs);
+		}
+		else
+			printf("i: ----  ");
+		if(ud->maxoutputs)
+		{
+			if(ud->minoutputs == ud->maxoutputs)
+				printf("o: %d     ", ud->minoutputs);
+			else
+				printf("o: %d..%d  ", ud->minoutputs,
+						ud->maxoutputs);
+		}
+		else
+			printf("o: ----  ");
+
+		/* Control registers */
+		for(crd = ud->registers; crd->name; crd++)
+			printf(" %s", crd->name);
+		break;
+	  }
+	  case A2_TSTRING:
+		printf("%s", a2_String(state, h));
+		break;
+	  case A2_TDETACHED:
+	  case A2_TVOICE:
+		break;
+	}
+	printf("\n");
+	if(has_exports)
+	{
+		for(i = 0; i < indent; ++i)
+			printf("    ");
+		printf("---------------------------------------------\n");
+		for(i = 0; (x = a2_GetExport(state, h, i)) >= 0; ++i)
+			print_info(indent + 1, a2_GetExportName(state, h, i), x);
+		for(i = 0; i < indent; ++i)
+			printf("    ");
+	}
+}
+
+
+/* Try to load each non-option argument as an .a2s file. */
+static void load_sounds(int argc, const char *argv[])
+{
+	int i;
+	for(i = 1; i < argc; ++i)
+	{
+		A2_handle h;
+		if(argv[i][0] == '-')
+			continue;
+		if((h = a2_Load(state, argv[i])) < 0)
+		{
+			fprintf(stderr, "Could not load \"%s\"! (%s)\n",
+					argv[i], a2_ErrorString(-h));
+			continue;
+		}
+		a2_Export(state, A2_ROOTBANK, h, NULL);
+		module = h;
+		fprintf(stderr, "Loaded \"%s\" - %s - %s\n",
+				a2_Name(state, h),
+				a2_String(state, a2_Get(state, h, "author")),
+				a2_String(state, a2_Get(state, h, "title")));
+	}
+}
+
+
+static void usage(const char *exename)
+{
+	unsigned v = a2_LinkedVersion();
+	fprintf(stderr, "Audiality 2 v%d.%d.%d.%d\n",
+			A2_MAJOR(v),
+			A2_MINOR(v),
+			A2_MICRO(v),
+			A2_BUILD(v));
+	fprintf(stderr, "Copyright 2010-2013 David Olofson\n\n");
+	fprintf(stderr, "Usage: %s [switches] <file>\n\n", exename);
+	fprintf(stderr, "Switches:  -d<name>    Select audio driver\n");
+	fprintf(stderr, "           -b<n>       Audio buffer size (frames)\n");
+	fprintf(stderr, "           -r<n>       Audio sample rate (Hz)\n");
+	fprintf(stderr, "           -c<n>       Number of audio channels\n");
+	fprintf(stderr, "           -p<name>[,arg[,arg[,...]]]\n");
+	fprintf(stderr, "                       Run program <name> with the "
+			"specified arguments\n");
+	fprintf(stderr, "           -x          Print module exports\n");
+	fprintf(stderr, "           -xr         Print engine root exports\n");
+	fprintf(stderr, "           -h          Help\n\n");
+}
+
+
+/* Parse driver selection and configuration switches */
+static void parse_args(int argc, const char *argv[])
+{
+	int i;
+	for(i = 1; i < argc; ++i)
+	{
+		if(argv[i][0] != '-')
+			continue;
+		if(strncmp(argv[i], "-d", 2) == 0)
+		{
+			free(audiodriver);
+			audiodriver = strdup(&argv[i][2]);
+			printf("[Audio driver: %s]\n", audiodriver);
+		}
+		else if(strncmp(argv[i], "-b", 2) == 0)
+		{
+			audiobuf = atoi(&argv[i][2]);
+			printf("[Audio buffer: %d]\n", audiobuf);
+		}
+		else if(strncmp(argv[i], "-r", 2) == 0)
+		{
+			samplerate = atoi(&argv[i][2]);
+			printf("[Audio sample rate: %d]\n", samplerate);
+		}
+		else if(strncmp(argv[i], "-c", 2) == 0)
+		{
+			samplerate = atoi(&argv[i][2]);
+			printf("[Audio channels: %d]\n", channels);
+		}
+		else if(strncmp(argv[i], "-p", 2) == 0)
+			continue;
+		else if(strncmp(argv[i], "-xr", 3) == 0)
+			continue;
+		else if(strncmp(argv[i], "-x", 2) == 0)
+			continue;
+		else if(strncmp(argv[i], "-h", 2) == 0)
+		{
+			usage(argv[0]);
+			exit(0);
+		}
+		else
+		{
+			fprintf(stderr, "Unknown switch '%s'!\n", argv[i]);
+			usage(argv[0]);
+			exit(1);
+		}
+	}
+}
+
+
+/* Parse the body of a -p switch; <name>[,pitch[,vel[,mod[,...]]]] */
+static int play_sound(const char *cmd)
+{
+	A2_handle h;
+	A2_errors res;
+	int i;
+	char program[256];
+	float a[A2_MAXARGS];
+	int ia[A2_MAXARGS];
+	int cnt;
+	printf("Playing %s/%s...\n", a2_Name(state, module), cmd);
+	cnt = sscanf(cmd, "%[A-Za-z0-9_],%f,%f,%f,%f,%f,%f,%f,%f", program,
+			a, a + 1, a + 2, a + 3, a + 4, a + 5, a + 6, a + 7);
+	if(cnt <= 0)
+	{
+		fprintf(stderr, "a2play: -p switch with no arguments!\n");
+		return -1;
+	}
+	if((h = a2_Get(state, module, program)) < 0)
+	{
+		fprintf(stderr, "a2play: a2_Get(\"%s\"): %s\n", program,
+				a2_ErrorString(-h));
+		return -2;
+	}
+	for(i = 0; i < cnt - 1; ++i)
+		ia[i] = a[i] * 65536.0f;
+	if((res = a2_Playa(state, a2_RootVoice(state), h, cnt - 1, ia)))
+	{
+		fprintf(stderr, "a2play: a2_Starta(): %s\n",
+				a2_ErrorString(res));
+		return -3;
+	}
+	return 1;
+}
+
+
+/*
+ * Handle any -p switches. If no -p switches are found, try the default entry
+ * point "Song".
+ *
+ * Returns 1 if one or more programs were started, a negative value if there
+ * was an error, otherwise 0.
+ */
+static int play_sounds(int argc, const char *argv[])
+{
+	int i;
+	int res = 0;
+	for(i = 1; i < argc; ++i)
+	{
+		if(argv[i][0] != '-')
+			continue;
+		if(strncmp(argv[i], "-p", 2) == 0)
+		{
+			if(play_sound(&argv[i][2]) < 0)
+				return -1;
+			res = 1;
+		}
+		else if(strncmp(argv[i], "-xr", 3) == 0)
+			print_info(0, NULL, A2_ROOTBANK);
+		else if(strncmp(argv[i], "-x", 2) == 0)
+			print_info(0, NULL, module);
+	}
+	if(!res && (a2_Get(state, module, "Song") >= 0))
+		res = play_sound("Song");
+	return res;
+}
+
+
+static void breakhandler(int a)
+{
+	fprintf(stderr, "a2play: Stopping...\n");
+	do_exit = 1;
+}
+
+
+static void fail(A2_errors err)
+{
+	fprintf(stderr, "ERROR: %s\n", a2_ErrorString(err));
+	exit(100);
+}
+
+
+int main(int argc, const char *argv[])
+{
+	A2_config *cfg;
+	float a;
+	signal(SIGTERM, breakhandler);
+	signal(SIGINT, breakhandler);
+
+	/* Command line switches */
+	if(argc <= 1)
+	{
+		fprintf(stderr, "No arguments specified!\n");
+		usage(argv[0]);
+		exit(1);
+	}
+	parse_args(argc, argv);
+
+	/* Configure and open engine */
+	if(!(cfg = a2_OpenConfig(samplerate, audiobuf, 2,
+			a2flags | A2_STATECLOSE)))
+		fail(a2_LastError());
+	if(audiodriver)
+		if(a2_AddDriver(cfg, a2_NewDriver(A2_AUDIODRIVER, audiodriver)))
+			fail(a2_LastError());
+	if(!(state = a2_Open(cfg)))
+		fail(a2_LastError());
+
+	/* Load sounds */
+	load_sounds(argc, argv);
+
+	/* Start playing! */
+	a2_Now(state);
+	if(play_sounds(argc, argv) == 1)
+	{
+		/* Wait for completion or abort */
+		while(!do_exit)
+		{
+			a2_Now(state);
+			sleep(1);
+		}
+
+		/* Fade out */
+		a2_Now(state);
+		a = 1.0f;
+		while(a > 0.01f)
+		{
+			a2_Send(state, a2_RootVoice(state), 2, a);
+			a2_Wait(state, 100.0f);
+			a *= 0.5f;
+		}
+		a2_Send(state, a2_RootVoice(state), 2, 0.0f);
+		sleep(1);
+	}
+
+	/* Close and clean up */
+	a2_Close(state);
+	free(audiodriver);
+	return 0;
+}
